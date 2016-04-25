952df56d801359ccdf6554685705680c7b5f6c21
Jenna Warren <warren.jenna@gmail.com>
2016-04-25 20:40:44 +0200

    i change private to public

--- app/src/main/java/com/example/calvin/weatherapp/YearlyCityData.java
+++ app/src/main/java/com/example/calvin/weatherapp/YearlyCityData.java
@@ -11,14 +11,14 @@ public class YearlyCityData {
     private int year;
 
     // Each of the array lists holds data for each day of the year
-    private ArrayList<Double> rainfall; // centimeters
+    public ArrayList<Double> rainfall; // centimeters
 
     // All temperatures celcius
-    private ArrayList<Double> temperatureHighs;
-    private ArrayList<Double> temperatureLows;
-    private ArrayList<Double> temperatureAverages;
-    private ArrayList<Double> humidity; // make up your own scale?
-    private ArrayList<Double> clouds; // 0 is sunny, 100 is very cloudy
+    public ArrayList<Double> temperatureHighs;
+    public ArrayList<Double> temperatureLows;
+    public ArrayList<Double> temperatureAverages;
+    public ArrayList<Double> humidity; // make up your own scale?
+    public ArrayList<Double> clouds; // 0 is sunny, 100 is very cloudy
 
     public void setRainfallData(ArrayList<Double> rainfallData) {
         this.rainfall = rainfallData;
