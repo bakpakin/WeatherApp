3ebfab1a9d53ae229eccfec66f7f118154663264
Calvin Rose <calsrose@gmail.com>
2016-04-25 15:16:16 -0400

    Add volley. Start to flesh out backend connector.

--- app/src/main/java/com/example/calvin/weatherapp/BackendConnector.java
+++ app/src/main/java/com/example/calvin/weatherapp/BackendConnector.java
@@ -1,16 +1,47 @@
 package com.example.calvin.weatherapp;
 
+import com.android.volley.Network;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.BasicNetwork;
+import com.android.volley.toolbox.HurlStack;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
+
+import android.content.Context;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
 import java.util.ArrayList;
 
 /**
  * Created by Calvin on 4/23/16.
  */
-public class BackendConnector {
+public class BackendConnector  {
+
+    private static final String apibase = "https://cowweather.herokuapp.com/";
+    private static BackendConnector instance = null;
+
+    private RequestQueue requestQueue;
 
     private ArrayList<BackendListener> listeners;
 
-    public BackendConnector() {
+    public static void initInstance(Context c) {
+        instance = new BackendConnector(c);
+    }
 
+    public static BackendConnector getInstance() {
+        if (instance == null) {
+            System.err.println("Backend connector singleton instance not initialized.");
+        }
+        return instance;
+    }
+
+    private BackendConnector(Context c) {
+        requestQueue = Volley.newRequestQueue(c);
     }
 
     public BackendListener addListener(BackendListener listener) {
@@ -18,13 +49,64 @@ public class BackendConnector {
         return listener;
     }
 
-    public void getCityDataByName(String name, int year) {
+    private YearlyCityData parseHistoryData(JSONObject obj) throws JSONException {
+        // TODO
+        // This is the meat of this class. Converts the JSON data into a YearlyCityObject.
+        return null;
+    }
 
+    public void getCityDataByName(String name, int year) {
+        final BackendConnector outerThis = this;
+        JsonObjectRequest jsonRequest = new JsonObjectRequest
+                (Request.Method.GET, apibase + name, null, new Response.Listener<JSONObject>() {
+                    @Override
+                    public void onResponse(JSONObject response) {
+                        try {
+                            // Parse the returned JSON object here...
+                            System.out.println(response);
+                            YearlyCityData data = outerThis.parseHistoryData(response);
+                            for (BackendListener listener : listeners) {
+                                listener.onCityDataReceived(data);
+                            }
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                }, new Response.ErrorListener() {
+
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        error.printStackTrace();
+                    }
+                });
+        this.requestQueue.add(jsonRequest);
     }
 
     public void getCityDataByID(int id, int year) {
+        final BackendConnector outerThis = this;
+        JsonObjectRequest jsonRequest = new JsonObjectRequest
+                (Request.Method.GET, apibase + id, null, new Response.Listener<JSONObject>() {
+                    @Override
+                    public void onResponse(JSONObject response) {
+                        try {
+                            // Parse the returned JSON object here...
+                            System.out.println(response);
+                            YearlyCityData data = outerThis.parseHistoryData(response);
+                            for (BackendListener listener : listeners) {
+                                listener.onCityDataReceived(data);
+                            }
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                }, new Response.ErrorListener() {
 
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        error.printStackTrace();
+                    }
+                });
+        this.requestQueue.add(jsonRequest);
     }
-
 
 }
--- app/src/main/java/com/example/calvin/weatherapp/MainActivity.java
+++ app/src/main/java/com/example/calvin/weatherapp/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.calvin.weatherapp;
 
+import android.support.v4.app.FragmentManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -7,6 +8,17 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        BackendConnector.initInstance(this);
+
+        // Test the Backend Connector:
+        BackendConnector.getInstance().addListener(new BackendListener() {
+            @Override
+            public void onCityDataReceived(YearlyCityData data) {
+
+            }
+        });
+        BackendConnector.getInstance().getCityDataByName("Boston", 1970);
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
